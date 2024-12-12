2c817bddf0fbd5e813c12b7a1e6ae2376091e200
MarianaVinyolas <marianavinyolas@gmail.com>
2024-12-12 13:50:51 -0300

    ADD: multilanguage;

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { Router } from '@router'
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import { PagesLang } from '@pages'
+import './styles/globals.css'
 
-createRoot(document.getElementById('root')!).render(
+
+i18next.use(initReactI18next).init({
+	lng: 'es',
+	fallbackLng: 'en',
+
+	resources: {
+		es: {
+			Pages: PagesLang?.es,
+
+		},
+		en: {
+			Pages: PagesLang?.en,
+		},
+	},
+
+	interpolation: {
+		escapeValue: false,
+	},
+})
+
+const currentLanguage = localStorage.getItem('LANGUAGE')
+currentLanguage && i18next.changeLanguage(currentLanguage)
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </StrictMode>,
 )
