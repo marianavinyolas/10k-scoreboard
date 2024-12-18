3224e2ac1e21d3c756a4780dfaa785132910f711
MarianaVinyolas <marianavinyolas@gmail.com>
2024-12-18 19:25:43 -0300

    ADD: setIsDone prop to flag the effect ending

--- src/components/molecules/TypingMachine.tsx
+++ src/components/molecules/TypingMachine.tsx
@@ -1,13 +1,19 @@
 import { FC, useEffect, useState } from 'react'
 
 interface TypingMachineProps {
-	text: string // The string to type out
-	speed?: number // Time delay between letters in milliseconds
-	className?:string
+	text: string
+	speed?: number
+	className?: string
+	setIsDone: (value: boolean) => void
 }
-const TypingMachine: FC<TypingMachineProps> = ({ text, speed = 100, className }) => {
-	const [displayedText, setDisplayedText] = useState('') // For showing typed characters
-	const [index, setIndex] = useState(0) // Tracks the current character index
+const TypingMachine: FC<TypingMachineProps> = ({
+	text,
+	speed = 100,
+	className,
+	setIsDone,
+}) => {
+	const [displayedText, setDisplayedText] = useState(text.charAt(0))
+	const [index, setIndex] = useState(1)
 
 	useEffect(() => {
 		// Typing effect interval
@@ -15,6 +21,8 @@ const TypingMachine: FC<TypingMachineProps> = ({ text, speed = 100, className })
 			if (index < text.length) {
 				setDisplayedText(prev => prev + text.charAt(index))
 				setIndex(prev => prev + 1)
+			} else {
+				setIsDone(true)
 			}
 		}, speed)
 
