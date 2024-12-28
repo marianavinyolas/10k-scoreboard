7f57b7f3dcec2d98449183696fedd4f2fd19f791
MarianaVinyolas <marianavinyolas@gmail.com>
2024-12-28 20:53:02 -0300

    ADD: conditional Nav link buttons

--- src/components/atoms/Icons/IcChevron.tsx
+++ src/components/atoms/Icons/IcChevron.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import type { IIconProps } from './Icons.interfaces'
 
-export const IcChevron = ({ className }: IIconProps) => (
+ const IcChevron = ({ className }: IIconProps) => (
 	<svg
 		viewBox='0 0 11 6'
 		xmlns='http://www.w3.org/2000/svg'
@@ -9,3 +10,5 @@ export const IcChevron = ({ className }: IIconProps) => (
 		<path d='M5.71875 0.125C5.875 -0.03125 6.09375 -0.03125 6.25 0.125L10.875 4.71875C11.0312 4.84375 11.0312 5.09375 10.875 5.25L10.25 5.84375C10.125 6 9.875 6 9.71875 5.84375L6 2.15625L2.25 5.84375C2.09375 6 1.875 6 1.71875 5.84375L1.09375 5.25C0.9375 5.09375 0.9375 4.84375 1.09375 4.71875L5.71875 0.125Z' />
 	</svg>
 )
+
+export default React.memo(IcChevron)
--- src/components/organisms/GameHeader.tsx
+++ src/components/organisms/GameHeader.tsx
@@ -1,16 +1,17 @@
-import { IcSettings, IcTrophy } from '@atoms'
+import { IcDices, IcSettings, IcTrophy } from '@atoms'
 import i18next from 'i18next'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const GameHeader = () => {
 	const navigate = useNavigate()
-	
+	const { pathname } = useLocation()
+
 	useEffect(() => {
 		const currentLanguage = localStorage.getItem('LANGUAGE') || 'es'
 		i18next.changeLanguage(currentLanguage)
 	}, [])
-	
+
 	useEffect(() => {
 		const currentTheme = localStorage.getItem('THEME') || 'light'
 		document.documentElement.classList.toggle('dark', currentTheme === 'dark')
@@ -19,18 +20,30 @@ const GameHeader = () => {
 	return (
 		<header className='w-full'>
 			<section className='flex justify-end gap-2'>
-				<button
-					className='cursor-pointer w-7 h-7 flex items-center justify-center  rounded border border-neutral-500 dark:border-neutral-300'
-					onClick={() => navigate('/settings')}
-				>
-					<IcSettings className='w-6 h-6 fill-neutral-700 dark:fill-neutral-300' />
-				</button>
-				<button
-					className='cursor-pointer w-7 h-7 flex items-center justify-center  rounded border border-neutral-500 dark:border-neutral-300'
-					onClick={() => navigate('/leaderboard')}
-				>
-					<IcTrophy className='w-6 h-6 fill-neutral-700 dark:fill-neutral-300' />
-				</button>
+				{!pathname.includes('settings') && (
+					<button
+						className='cursor-pointer w-7 h-7 flex items-center justify-center  rounded border border-neutral-500 dark:border-neutral-300'
+						onClick={() => navigate('/settings')}
+					>
+						<IcSettings className='w-6 h-6 fill-neutral-700 dark:fill-neutral-300' />
+					</button>
+				)}
+				{pathname.includes('game') && (
+					<button
+						className='cursor-pointer w-7 h-7 flex items-center justify-center  rounded border border-neutral-500 dark:border-neutral-300'
+						onClick={() => navigate('/leaderboard')}
+					>
+						<IcTrophy className='w-6 h-6 fill-neutral-700 dark:fill-neutral-300' />
+					</button>
+				)}
+				{pathname.includes('leaderboard') && (
+					<button
+						className='cursor-pointer w-7 h-7 flex items-center justify-center  rounded border border-neutral-500 dark:border-neutral-300'
+						onClick={() => navigate('/game')}
+					>
+						<IcDices className='w-6 h-6 fill-neutral-700 dark:fill-neutral-300' />
+					</button>
+				)}
 			</section>
 		</header>
 	)
