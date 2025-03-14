b6dac94a4a62a8ef3678715021ae781a40377055
MarianaVinyolas <marianavinyolas@gmail.com>
2025-03-14 16:21:10 -0300

    ADD: component interface

--- src/components/molecules/InactivePlayer.tsx
+++ src/components/molecules/InactivePlayer.tsx
@@ -1,10 +1,14 @@
-import { IPlayer } from '@interfaces'
+interface IInactivePlayer {
+	name: string
+	score: number
+	playerIndex: number
+}
 
-const InactivePlayer = ({ name, id, score }: IPlayer) => {
+const InactivePlayer = ({ name, score, playerIndex }: IInactivePlayer) => {
 	return (
-		<article className='w-[80%] flex gap-2 py-2 border border-slate-500 rounded bg-gray-100 dark:bg-slate-800'>
+		<article className='w-full flex gap-2 py-2 border border-slate-500 rounded bg-gray-100 dark:bg-slate-800'>
 			<div className='w-10 flex justify-center border-r border-slate-500'>
-				{id + 1}
+				{playerIndex}
 			</div>
 			<div className='w-20 flex justify-end pr-2 border-r border-slate-500'>
 				{score}
