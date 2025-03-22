cb2392bc997f39f9819f1ffc091d2b5f00a3a62c
MarianaVinyolas <marianavinyolas@gmail.com>
2025-03-22 18:01:34 -0300

    ADD: vite config deploy script

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+// https://vite.dev/config/
+export default defineConfig({
+    base: '/10k-scoreboard/',
+    plugins: [react(), tsconfigPaths()],
+});
