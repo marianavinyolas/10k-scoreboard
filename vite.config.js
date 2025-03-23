9906bb1f62320bdcbe0c2ad2d10656cbbf265e2b
MarianaVinyolas <marianavinyolas@gmail.com>
2025-03-23 16:47:19 -0300

    UNCOMMENT: enable base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
-    // base: '/10k-scoreboard/',
+    base: '/10k-scoreboard/',
     plugins: [react(), tsconfigPaths()],
 });
