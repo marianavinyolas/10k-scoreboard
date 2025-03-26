d3c21f83504102f73151a8a9095bc8d71f3ed1f4
MarianaVinyolas <marianavinyolas@gmail.com>
2025-03-26 12:43:41 -0300

    UPDATE: initialize scoresList with default player ranking if no scores are found in localStorage; maintain sorting for existing scores

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -11,18 +11,28 @@ const LeaderboardPage = () => {
 	const [scoresList, setScoresList] = useState<IPlayer[]>([])
 	const [, setIsDone] = useState(false)
 
-
 	useEffect(() => {
 		const scores = JSON.parse(localStorage.getItem('SCORES') || '[]')
 		const winners = JSON.parse(localStorage.getItem('WINNER') || '[]')
 
-		const sortedScores = scores.sort(
-			(a: IPlayer, b: IPlayer) => b.score - a.score
-		)
+		if (scores.length === 0) {
+			let ranking = JSON.parse(localStorage.getItem('PLAYERS') || '[]')
+			ranking = ranking.map((name: string, index: number) => ({
+				name,
+				active: index === 0,
+				score: 0,
+				id: index,
+			}))
+			setScoresList(ranking)
+		} else {
+			const sortedScores = scores.sort(
+				(a: IPlayer, b: IPlayer) => b.score - a.score
+			)
 
-		const ranking = [...winners, ...sortedScores]
+			const ranking = [...winners, ...sortedScores]
 
-		setScoresList(ranking)
+			setScoresList(ranking)
+		}
 	}, [])
 
 	const hdlResetGame = () => {
@@ -37,13 +47,13 @@ const LeaderboardPage = () => {
 		<main className='w-screen h-dvh  text-neutral-700 dark:text-neutral-200 flex flex-col gap-[3vh] items-center px-2 sm:px-[5vw] py-[3vh]'>
 			<GameHeader reset={hdlResetGame} />
 			<article className='w-full flex flex-col items-center gap-8'>
-					<TypingMachine
-						text={`${t('leaderboardPage.title')}`}
-						speed={150}
-						className='text-3xl lg:text-5xl'
-						{...{ setIsDone }}
-					/>
-				</article>
+				<TypingMachine
+					text={`${t('leaderboardPage.title')}`}
+					speed={150}
+					className='text-3xl lg:text-5xl'
+					{...{ setIsDone }}
+				/>
+			</article>
 
 			<section className='w-full sm:w-[60%] lg:w-[50%] h-full flex flex-col items-center gap-2 sm:gap-4 lg:gap-8'>
 				{scoresList.map((player, idx) => (
