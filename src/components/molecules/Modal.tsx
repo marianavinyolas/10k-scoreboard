d9ea70dec25b9ed3bd57c30ea141a755695eae7e
MarianaVinyolas <marianavinyolas@gmail.com>
2025-09-04 15:25:27 -0300

    REFINE: Enhance Modal component by adding optional className prop for improved styling flexibility.

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -4,9 +4,10 @@ interface IModalProps {
 	isOpen: boolean
 	// onClose?: () => void
 	children: React.ReactNode
+	className?: string
 }
 
-const Modal = ({ isOpen, children }: IModalProps) => {
+const Modal = ({ isOpen, children, className }: IModalProps) => {
 	const dialogRef = useRef<HTMLDialogElement>(null)
 
 	useLayoutEffect(() => {
@@ -19,7 +20,7 @@ const Modal = ({ isOpen, children }: IModalProps) => {
 	return (
 		<dialog
 			ref={dialogRef}
-			className='w-[95%] sm:w-[70%] lg:w-[50%] rounded-md bg-neutral-100 dark:bg-slate-600 p-4 backdrop:backdrop-blur-sm'
+			className={`w-[95%] sm:w-[70%] lg:w-[50%] rounded-md bg-neutral-100 dark:bg-slate-600 p-4 backdrop:backdrop-blur-sm ${className}`}
 		>
 			{children}
 		</dialog>
